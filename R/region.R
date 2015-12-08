cea79a563804eaf0124755e513b5abb6b5e4dc93
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-08 23:10:55 +0100

    improved region() to accept region name or key

--- R/region.R
+++ R/region.R
@@ -1,10 +1,15 @@
 #' Get a region shp file
 #'
 #' @export
-#' @param x Region name
-#' @param format one of geojson, shp
+#' @param name (character) Region name
+#' @param key (character) Region key
+#' @param format (character) One of 'geojson' or 'shp'
 #' @examples \dontrun{
-#' res <- region(x = "MarineRegions:eez_33176", format = "geojson")
+#' # by key
+#' res <- region(key = "MarineRegions:eez_33176", format = "geojson")
+#'
+#' # by name
+#' res <- region(name = "Turkmen Exclusive Economic Zone", format = "geojson")
 #'
 #' library("geojsonio")
 #' as.json(res) %>% map_leaf
@@ -12,11 +17,21 @@
 #' nms <- region_names()
 #' as.json(region(nms[[1]]$name)) %>% map_leaf
 #' }
-region <- function(x, format = "geojson") {
+region <- function(name = NULL, key = NULL, format = "geojson") {
   format <- match.arg(format, c('geojson', 'shp'))
+  key <- nameorkey(name, key)
   args <- list(service = 'WFS', version = '1.0.0', request = 'GetFeature',
-               typeName = x, maxFeatures = 50,
-               outputFormat = "application/json")
+               typeName = key, maxFeatures = 50, outputFormat = "application/json")
   res <- m_GET(vliz_base(), args)
   jsonlite::fromJSON(res, FALSE)
+}
+
+nameorkey <- function(name, key) {
+  stopifnot(xor(!is.null(name), !is.null(key)))
+  if (is.null(key)) {
+    nms <- region_names()
+    nms[vapply(nms, "[[", "", "title") == name][[1]]$name
+  } else {
+    key
+  }
 }
