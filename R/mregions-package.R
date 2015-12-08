4f8d954b5280dfb532c39ef67be228c0d4a38105
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-08 16:45:32 +0100

    first commit

---
+++ R/mregions-package.R
@@ -0,0 +1,9 @@
+#' Marine regions data
+#'
+#' @import httr jsonlite
+#' @name mregions-package
+#' @aliases mregions
+#' @docType package
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
---
+++ R/region_names.R
@@ -0,0 +1,7 @@
+#' Get region names
+#'
+#' @export
+#' @param x Region name
+region_names <- function(x) {
+  print("hello world")
+}
