0c002930c7be61c153a1a731d61a18d578c77107
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-08 18:35:22 +0100

    region names fxn

--- R/region_names.R
+++ R/region_names.R
@@ -2,6 +2,27 @@
 #'
 #' @export
 #' @param x Region name
+#' @examples \dontrun{
+#' region_names()
+#' }
 region_names <- function(x) {
-  print("hello world")
+  args <- list(SERVICE = 'WFS', REQUEST = 'GetCapabilities')
+  res <- m_GET(vliz_base(), args)
+  xml <- xml2::read_xml(res)
+  features <- xml_children(xml_children(xml)[[4]])
+  lapply(features, function(z) {
+    list(name = ex_name(z, 1), title = ex_name(z, 2))
+  })
+}
+
+vliz_base <- function(x) "http://geo.vliz.be/geoserver/MarineRegions/ows"
+
+m_GET <- function(url, args) {
+  tt <- GET(url, query = args)
+  stop_for_status(tt)
+  content(tt, "text")
+}
+
+ex_name <- function(x, y) {
+  xml_text(xml_children(x)[[y]])
 }
