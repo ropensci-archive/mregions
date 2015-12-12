2d5932f3f95d466a190892f0670a73ae97e77a2e
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-12 11:40:36 -0800

    bumped dev version, fixed param names lines

--- R/region_shp.R
+++ R/region_shp.R
@@ -7,9 +7,9 @@
 #' @param path (character) path to write shp files to, only used in \code{region_shp}
 #' @param overwrite (logical) Overwrite file if already exists. Default: \code{FALSE}
 #' @param read (logical) To read in as spatial object. If \code{FALSE} a path
-#' @param filter (character) String to filter features on
 #' given back. if \code{TRUE}, you need the \code{rgdal} package installed.
 #' Default: \code{FALSE}
+#' @param filter (character) String to filter features on
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
 #' @examples \dontrun{
 #' ## just get path
@@ -17,10 +17,16 @@
 #' ## read shp file into spatial object
 #' res <- region_shp(key = "MarineRegions:eez_33176", read = TRUE)
 #' library('leaflet')
-#' leaflet() %>% addPolygons(data = res)
+#' leaflet() %>%
+#'   addProviderTiles(provider = "Stamen.TonerHybrid") %>%
+#'   addPolygons(data = res)
+#'
+#' # use `filter` param to get a subset of a region
+#' region_shp(name="World Marine Heritage Sites", maxFeatures=NULL,
+#'   filter="iSimangaliso Wetland Park")
 #' }
-region_shp <- function(name = NULL, key = NULL, maxFeatures = 50,
-                       path = "~/.mregions", overwrite = TRUE, read = TRUE, filter = NULL, ...) {
+region_shp <- function(name = NULL, key = NULL, maxFeatures = 50, path = "~/.mregions",
+                       overwrite = TRUE, read = TRUE, filter = NULL, ...) {
 
   args <- make_args('shp', name, key, maxFeatures)
   res <- m_GET(vliz_base(), args, path, overwrite, ...)
