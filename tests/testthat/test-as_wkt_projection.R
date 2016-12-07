26fae65040b5a9a948aefe870c3ab644b6c4250b
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-07 10:31:56 -0800

    skip on cran as_wkt tests, bump to patch ver. near to cran push

--- tests/testthat/test-as_wkt_projection.R
+++ tests/testthat/test-as_wkt_projection.R
@@ -1,6 +1,8 @@
 context("wkt projection")
 
 test_that("conversion to wkt results in longlat data", {
+  skip_on_cran()
+
   shpfile <- mr_shp(key = "MarineRegions:eez_iho_union_v2", read = FALSE)
   x <- rgdal::readOGR(dirname(shpfile), gsub("\\.shp", "", basename(shpfile)), verbose = FALSE)
   x1 <- sp::spTransform(x, "+proj=laea +ellps=WGS84 +lon_0=2.5 +lat_0=51.6")
@@ -9,4 +11,3 @@ test_that("conversion to wkt results in longlat data", {
   expect_match(wkt, 'GEOMETRYCOLLECTION')
   expect_match(wkt, 'POLYGON')
 })
-
